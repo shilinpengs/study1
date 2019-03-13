68e01e8bbb5ba71f483673dbe891304d5a1b161a
shilinpeng_bendi <18800103205@163.com>
2019-03-13 15:16:34 +0800

    dwa

--- routes/admin.php
+++ routes/admin.php
@@ -5,9 +5,9 @@
  * Date: 13/3/19
  * Time: 下午2:55
  */
-//Route::group(['prefix' => 'Admin', 'namespace' => 'Admin'], function() {
-//    Route::get('index', 'IndexController@index');
-//    Route::get('test', function() {
-//        return 'your route is ready';
-//    });
-//});
+Route::group(['prefix' => 'Admin', 'namespace' => 'Admin'], function() {
+    Route::get('index', 'IndexController@index');
+    Route::get('test', function() {
+        return 'your route is ready';
+    });
+});
