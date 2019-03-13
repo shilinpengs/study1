113ea6e76e6c1e23dc703c1576994d0f1f359ebe
shilinpeng_bendi <18800103205@163.com>
2019-03-13 15:18:25 +0800

    dwa

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,10 @@
  * Date: 13/3/19
  * Time: 下午2:55
  */
-Route::group(['prefix' => 'Admin', 'namespace' => 'Admin'], function() {
+
+
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function() {
     Route::get('index', 'IndexController@index');
     Route::get('test', function() {
         return 'your route is ready';
