ece8749e3a1e06bd82828143e9748fd27abf5b84
shilinpeng_bendi <18800103205@163.com>
2019-05-13 11:59:23 +0800

    dwa

---
+++ app/Http/Controllers/Api/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+
+    public function index() {
+
+        echo 'index';
+
+    }
+
+    public function test() {
+
+        echo 'test';
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'Api', 'namespace' => 'Api'], function() {
+    Route::get('index', 'IndexController@index');
+    Route::get('test', function() {
+        return 'your route is ready';
+    });
+});
